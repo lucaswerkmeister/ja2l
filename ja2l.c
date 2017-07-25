66735f3c808d48caab444a9596dd239aabd4ff80
Lucas Werkmeister <mail@lucaswerkmeister.de>
2017-07-25 23:35:07 +0200

    Implement array-to-lines functionality
    Don’t print the first (‘[’) and last (‘]’) line, and remove the trailing
    comma from every line except the second-to-last. (In fact, it’s a bit
    backwards – we use the absence of the trailing comma to determine that
    it’s the last item and therefore the second-to-last line.)

--- ja2l.c
+++ ja2l.c
@@ -6,6 +6,7 @@
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "cleanup.h"
@@ -25,9 +26,54 @@ int main(int argc, char *argv[]) {
   growPipe(STDIN_FILENO);
   growPipe(STDOUT_FILENO);
 
+  // read and discard first line
+  read = getline(&line, &len, input);
+  if (read == -1) {
+    error(0, 0, "malformed input: empty");
+    return 1;
+  }
+  if (strncmp("[\n", line, read+1) != 0) {
+    error(0, 0, "malformed input: first line is not a single left bracket (‘[’)");
+    return 1;
+  }
+
+  // copy array element lines
   while ((read = getline(&line, &len, input)) != -1) {
-    line[read-2] = 0;
-    printf("%s\n", line);
+    if (read < 2) {
+      error(0, 0, "malformed input: input line too short");
+      return 1;
+    }
+    if (line[read-2] == ',') {
+      // normal line, trim trailing comma
+      line[read-2] = 0;
+      printf("%s\n", line);
+    } else {
+      if (line[read-1] == '\n') {
+        // last line of array
+        printf("%s", line);
+        break;
+      } else {
+        error(0, 0, "malformed input: premature EOF");
+        return 1;
+      }
+    }
+  }
+
+  // read and discard last line
+  read = getline(&line, &len, input);
+  if (read == -1) {
+    error(0, 0, "malformed input: final right bracket (‘]’) missing");
+    return 1;
+  }
+  if (strncmp("]\n", line, read+1) != 0) {
+    error(0, 0, "malformed input: last line is not a single right bracket (‘]‘)");
+    return 1;
+  }
+
+  read = getline(&line, &len, input);
+  if (read != -1) {
+    error(0, 0, "malformed input: extra input after end of array");
+    return 1;
   }
 
   return 0;
